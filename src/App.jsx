29d57eb91972743bdae197303fb2c2bd34dfd2c2
Muhammad ali <muhammadali778888@gmail.com>
2026-04-14 15:22:47 +0500

    tugadi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/hero/Hero";
-
+import Section4 from "./components/Section4/Section4"
+import Section5 from "./components/Section4/Section5/Section5"
+import Footer from "./components/Section4/Footer/footer"
 const App = () => {
     return (
         <div>
             <Header />
             <Hero />
+            <Section4 />
+            <Section5 />
+            <Footer />
         </div>
     );
 };
